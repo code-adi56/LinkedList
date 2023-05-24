b0688d516ddd7bb252f657519ffdcd10ccacf061
Adishree <120395723+code-adi56@users.noreply.github.com>
2023-05-25 00:02:16 +0530

    Update Activityselection.java

--- Activityselection.java
+++ Activityselection.java
@@ -17,7 +17,7 @@ class Activityselection{
     public static void main(String args[])
     {
         int s[]={1,3,0,5,8,5};
-        int f[]={2,4,6,7,9,9};
+        int f[]={2,9,8,3,7,3};
         int n= s.length;
         printMaxActivities(s, f, n);
     }
